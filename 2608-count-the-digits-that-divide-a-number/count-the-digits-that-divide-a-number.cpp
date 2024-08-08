7e7d381693f2dd09d5ba29d91e81fc8b07f64f84
moiz200125 <130084727+moiz200125@users.noreply.github.com>
2024-08-08 14:11:37 +0500

    Time: 3 ms (14.83%) | Memory: 7.5 MB (17.20%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
@@ -7,7 +7,9 @@ public:
         while(n != 0)
         {
             int d = n % 10;
-            if(num % d == 0)
+            
+            // Skip division by zero
+            if(d != 0 && num % d == 0)
                 count++;
 
             n = n / 10;    
@@ -15,3 +17,22 @@ public:
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     int countDigits(int num) 
+//     {
+//         int count = 0;
+//         int n = num;
+//         while(n != 0)
+//         {
+//             int d = n % 10;
+//             if(num % d == 0)
+//                 count++;
+
+//             n = n / 10;    
+//         }
+//         return count;
+//     }
+// };
